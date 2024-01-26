e0f3e11e482b146d17a3f49a2bd78aa1bcc493f1
Ivan Cherkas <cherkas.ivan13@gmail.com>
2024-01-26 07:24:24 +0300

    init develop with base configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, LayoutComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'cv_2.0';
+    title = 'cv_2.0';
 }
