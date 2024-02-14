fa2baa41074ac72cf40e0762f66538e8ca74380d
Ivan Cherkas <cherkas.ivan13@gmail.com>
2024-02-14 13:27:29 +0300

    main-page

--- src/app/layout/components/footer/footer.component.spec.ts
+++ src/app/layout/components/footer/footer.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterLinksComponent } from './footer.component';
+import { FooterComponent } from './footer.component';
 
 describe('FooterLinksComponent', () => {
-    let component: FooterLinksComponent;
-    let fixture: ComponentFixture<FooterLinksComponent>;
+    let component: FooterComponent;
+    let fixture: ComponentFixture<FooterComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [FooterLinksComponent],
+            imports: [FooterComponent],
         }).compileComponents();
 
-        fixture = TestBed.createComponent(FooterLinksComponent);
+        fixture = TestBed.createComponent(FooterComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
