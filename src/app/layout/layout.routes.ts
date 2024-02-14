4aea04247de8b75b74165349432232efff6ef716
Ivan Cherkas <cherkas.ivan13@gmail.com>
2024-02-14 13:27:29 +0300

    main-page

--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -1,10 +1,42 @@
 import { Routes } from '@angular/router';
 
+import { ERoute } from '@app/core/enum/route.enum';
+
 import { LayoutComponent } from './layout.component';
 
 export const LAYOUT_ROUTES: Routes = [
     {
         path: '',
         component: LayoutComponent,
+        children: [
+            // {
+            //     path: ERoute.AUTH,
+            //     loadComponent: () =>
+            //         import('./pages/auth/auth.component').then(
+            //             (c) => c.AuthComponent,
+            //         ),
+            // },
+            {
+                path: ERoute.PROJECTS,
+                loadComponent: () =>
+                    import('./pages/projects/projects.component').then(
+                        (c) => c.ProjectsComponent,
+                    ),
+            },
+            {
+                path: ERoute.EXPERIENCE,
+                loadComponent: () =>
+                    import('./pages/experience/experience.component').then(
+                        (c) => c.ExperienceComponent,
+                    ),
+            },
+            {
+                path: ERoute.TECHNOLOGIES,
+                loadComponent: () =>
+                    import('./pages/technologies/technologies.component').then(
+                        (c) => c.TechnologiesComponent,
+                    ),
+            },
+        ],
     },
 ];
