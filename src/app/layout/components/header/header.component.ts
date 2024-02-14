8df756cbbf93f2be77769c2915be26e9e4fc187a
Ivan Cherkas <cherkas.ivan13@gmail.com>
2024-02-14 16:32:07 +0300

    init firebase

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cv-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'cv-header',
+    standalone: true,
+    imports: [],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
