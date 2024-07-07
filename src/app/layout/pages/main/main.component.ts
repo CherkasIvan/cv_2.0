8b21752ce642eaea6d7a0a7506176bd7dff7c16e
Ivan <cherkas.ivan13@gmail.com>
2024-07-07 12:26:37 +0300

    added animations for blobs and update firbase data about mainpage

--- src/app/layout/pages/main/main.component.ts
+++ src/app/layout/pages/main/main.component.ts
@@ -1,7 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
+
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    OnDestroy,
+    OnInit,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { ButtonComponent } from '@layout/components/button/button.component';
+
+import { IMainPageInfo } from '@app/core/models/main-page-info';
+import { FirebaseService } from '@app/core/service/firebase/firebase.service';
 
 import { ProfileLogoComponent } from '../../../layout/components/profile-logo/profile-logo.component';
 
@@ -13,4 +24,28 @@ import { ProfileLogoComponent } from '../../../layout/components/profile-logo/pr
     styleUrl: './main.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent {}
+export class MainComponent implements OnInit, OnDestroy {
+    public mainInfo$: Observable<IMainPageInfo> =
+        this._firebaseService.getMainPageInfo();
+    public mainInfoPageData: IMainPageInfo | null = null;
+
+    private destroyed$: Subject<void> = new Subject();
+
+    constructor(
+        private _firebaseService: FirebaseService,
+        private _cdr: ChangeDetectorRef,
+    ) {}
+
+    ngOnInit(): void {
+        this.mainInfo$.pipe(takeUntil(this.destroyed$)).subscribe((info) => {
+            this.mainInfoPageData = info;
+            console.log(this.mainInfoPageData);
+            this._cdr.markForCheck();
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.destroyed$.next();
+        this.destroyed$.complete();
+    }
+}
