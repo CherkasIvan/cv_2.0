e71e76fb5d452b9a480344f12b7af3d5a826910a
Ivan <cherkas.ivan13@gmail.com>
2024-08-12 14:28:28 +0300

    добавил авторизацию , но пока нет возможности разлогиниться. нужно будет поправить

---
+++ src/app/core/utils/guards/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Observable } from 'rxjs';
+
+import { Injectable } from '@angular/core';
+import { Router, UrlTree } from '@angular/router';
+
+import { ERoute } from '@core/enum/route.enum';
+import { AuthService } from '@core/service/auth/auth.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthGuard {
+    constructor(
+        private readonly _authService: AuthService,
+        private _router: Router,
+    ) {}
+    canActivate(): Observable<boolean> | Promise<boolean> | UrlTree | boolean {
+        if (!this._authService.isAuth$.value) {
+            this._router.navigate([ERoute.AUTH]);
+            return false;
+        }
+        return true;
+    }
+}
