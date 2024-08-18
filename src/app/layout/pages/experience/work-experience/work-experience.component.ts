4773ee20e89a9ca52e57d7faa49db82043657804
Ivan <cherkas.ivan13@gmail.com>
2024-08-18 14:31:25 +0300

    добавил стор и открыти модальных окон для опыта

--- src/app/layout/pages/experience/work-experience/work-experience.component.ts
+++ src/app/layout/pages/experience/work-experience/work-experience.component.ts
@@ -15,14 +15,7 @@ import { ExperienceDialogComponent } from '@app/layout/components/experience-dia
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WorkExperienceComponent {
-    public isModalOpen = false;
     public selectedTabWork = input.required<string>();
     public workExperience$ = input.required<IWorkExperience[] | null>();
     public theme = input<boolean | null>();
-
-    public getWorkExperience(data: IWorkExperience | null) {
-        if (data) {
-            this.isModalOpen = true;
-        }
-    }
 }
