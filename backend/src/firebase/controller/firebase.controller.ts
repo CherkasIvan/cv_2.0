937a607ec0bc84c746372168c1ba9b4548f866e5
Ivan <cherkas.ivan13@gmail.com>
2024-09-08 16:37:05 +0300

    remgiokl.rhmgl;

--- backend/src/firebase/controller/firebase.controller.ts
+++ backend/src/firebase/controller/firebase.controller.ts
@@ -60,8 +60,8 @@ export class FirebaseController {
 
   @Get('hard-skills-nav')
   async getHardSkillsNav() {
-    const hardkillsNav = await this.firebaseService.getHardSkillsNav();
-    return hardkillsNav;
+    const hardSkillsNav = await this.firebaseService.getHardSkillsNav();
+    return hardSkillsNav;
   }
 
   @Get('education-places')
