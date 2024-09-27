f055c5c27dd3ee38aaad3fc607531e4e4ff3a874
Ivan <cherkas.ivan13@gmail.com>
2024-09-27 18:29:18 +0300

    refactored few any

--- frontend/src/app/layout/store/language-selector-store/language-selector.selectors.ts
+++ frontend/src/app/layout/store/language-selector-store/language-selector.selectors.ts
@@ -1,6 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-import { TLanguages } from '../model/language-selector.type';
+import { TLanguages } from '../model/languages.type';
 
 export const languageFeatureSelector =
     createFeatureSelector<TLanguages>('language');
