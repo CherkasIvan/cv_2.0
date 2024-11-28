48c6eb1140adbceaaad6b1ca757adc4a213dd963
Cherkas.I <cherkas.ivan13@gmail.com>
2024-11-28 14:00:35 +0300

    workble main with correct routes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:4000'],
+    origin: [
+      'http://localhost:4200',
+      'http://localhost:4000',
+      'http://localhost',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
